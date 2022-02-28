2ab982039c696b4cdca04e5ac0094a12c2acd2c2
김스시 <57784879+omakasekim@users.noreply.github.com>
2022-02-28 12:50:59 +0900

    Update app.js

--- 220227/app.js
+++ 220227/app.js
@@ -1,30 +1,4 @@
-// const eventEmitter = require('events');
+https://youtu.be/vs0mbfrbTYI
 
-// class Person extends eventEmitter{
-//     constructor(name){
-//         super();
-//         this._name = name;
-//     }
-//     get name(){
-//         return this._name;
-//     }
 
-// }
 
-// let pedro = new Person('Pedro');
-// let christina = new Person('Christina');
-// pedro.on('name',()=>{
-//     console.log('My name is '+pedro.name);
-// });
-// christina.on('name',()=>{
-//     console.log('My name is '+christina.name);
-// });
-// pedro.emit('name');
-// christina.emit('name');
-const http = require('http');
-const fs = require('fs');
-http.createServer((req,res)=>{
-    const readStream = fs.createReadStream('./static/index.html');
-    res.writeHead(200,{'Content-type': 'text/html'});
-    readStream.pipe(res);
-}).listen(3000);
